9724719b5f71f706c521823732a914ae0788b68a
Alexander peczon <97857268+BakedSoups@users.noreply.github.com>
2023-12-22 15:06:21 -0800

    Broken
    everything is broken ahh

--- extract.py
+++ extract.py
@@ -1,45 +1,39 @@
 from pptx import Presentation
-import os 
+import os
+import json
 
-def create_presentation_from_text(text):
-    presentation = Presentation()
+# Load JSON data from file
+with open('test.json', 'r') as file:
+    data = json.load(file)
+    print(data)
 
-    # Split the text into slides
-    slides_text = text.split("\n\n")
+# Create a new PowerPoint presentation
+presentation = Presentation()
 
-    for i, slide_text in enumerate(slides_text):
-        # Create a new slide
-        slide_layout = presentation.slide_layouts[0]  # You can change the layout as needed
-        slide = presentation.slides.add_slide(slide_layout)
+# Iterate over the JSON data and add slides to the presentation
+for item in data:
+    layout_name = item['layout']
+    
+    # Check if the layout exists in the new presentation
+    existing_layouts = [layout.name for layout in presentation.slide_layouts]
+    if layout_name not in existing_layouts:
+        print(f"Error: Slide layout '{layout_name}' not found. Skipping slide.")
+        continue
 
-        # Add text to the slide
-        shapes = slide.shapes
-        textbox = shapes.add_textbox(left=0, top=0, width=1, height=1)
-        frame = textbox.text_frame
-        frame.text = slide_text.strip()
+    new_layout = presentation.slide_layouts.get_by_name(layout_name)
+    slide = presentation.slides.add_slide(new_layout)
 
-    return presentation
+    for shape_data in item['shapes']:
+        shape_type = shape_data['type']
 
-def save_presentation(presentation, file_path):
-    presentation.save(file_path)
+        # Check if shape type is valid (e.g., 14 is for title)
+        if shape_type != 14:
+            print(f"Error: Invalid shape type '{shape_type}'. Skipping shape.")
+            continue
 
-def main():
-    # Specify the path to your text file
-    currentdir = os.getcwd()
-    text_file_path = "path/to/your/output_text.txt"
+        # Add title to the title placeholder
+        title_shape = slide.shapes.title
+        title_shape.text = shape_data['text']
 
-    # Read the text from the file
-    with open(text_file_path, 'r', encoding='utf-8') as file:
-        text = file.read()
-
-    # Create a PowerPoint presentation from the text
-    new_presentation = create_presentation_from_text(text)
-
-    # Specify the path to save the new PowerPoint file
-    new_pptx_file_path = "path/to/your/new_presentation.pptx"
-
-    # Save the new PowerPoint presentation
-    save_presentation(new_presentation, new_pptx_file_path)
-
-if __name__ == "__main__":
-    main()
+# Save the presentation as a PPTX file
+presentation.save('output.pptx')
--- main.py
+++ main.py
@@ -1,28 +1,14 @@
 from pptx import Presentation
-import os 
+import os
 import json
-def extract_text_from_slide(slide):
-    text = ""
-    for i, shape in enumerate(slide.shapes):
-        if hasattr(shape, "text"):
-            text += f"Shape {i+1}: {shape.text}\n"
-    return text.strip()
 
-def find_class_name(slide):
-    for i in range(0, len(slide.shapes)):
-        if hasattr(slide.shapes[i], "text"):
-            print(slide.shapes[i].text)
-            if slide.shapes[i].text == "[CLASS + NUMBER]":
-                return i +1 
-    return False
-def pptx_to_json(pptx_path, json_path):
+def pptx_to_xml(pptx_path, xml_path):
     presentation = Presentation(pptx_path)
+    xml_data = []
 
-    # Extract relevant information from each slide
-    slides_data = []
-    for slide in presentation.slides:
+    for i, slide in enumerate(presentation.slides):
         slide_data = {
-            'layout': slide.slide_layout.name,
+            'layout_index': presentation.slide_layouts.index(slide.slide_layout),
             'shapes': []
         }
 
@@ -37,38 +23,32 @@ def pptx_to_json(pptx_path, json_path):
             }
             slide_data['shapes'].append(shape_data)
 
-        slides_data.append(slide_data)
+        xml_data.append(slide_data)
 
-    # Write the extracted data to a JSON file
-    with open(json_path, 'w') as json_file:
-        json.dump(slides_data, json_file, indent=2)
-def json_to_pptx(json_path, pptx_path):
-    with open(json_path, 'r') as json_file:
-        slides_data = json.load(json_file)
+    with open(xml_path, 'w') as xml_file:
+        json.dump(xml_data, xml_file, indent=2)
+
+def xml_to_pptx(xml_path, pptx_path):
+    with open(xml_path, 'r') as xml_file:
+        slides_data = json.load(xml_file)
 
     presentation = Presentation()
 
     for slide_data in slides_data:
-        layout_name = slide_data['layout']
+        layout_index = slide_data['layout_index']
 
-        # Check if the layout exists
-        if layout_name not in [layout.name for layout in presentation.slide_layouts]:
-            # If not, check if it's a default layout and add it
-            default_layouts = ['Title Slide', 'Title and Content', ...]  # Add default layout names
-            if layout_name in default_layouts:
-                presentation.slide_layouts.add_layout_by_name(layout_name)
-            else:
-                print(f"Error: Slide layout '{layout_name}' not found. Skipping slide.")
-                continue
+        try:
+            new_layout = presentation.slide_layouts[layout_index]
+        except IndexError:
+            print(f"Error: Slide layout with index {layout_index} not found. Skipping slide.")
+            continue
 
-        slide_layout = presentation.slide_layouts[layout_name]
-        slide = presentation.slides.add_slide(slide_layout)
+        slide = presentation.slides.add_slide(new_layout)
 
         for shape_data in slide_data['shapes']:
             shape_type = shape_data['type']
 
-            # Check if shape type is valid (e.g., 1 is for rectangles)
-            if shape_type not in [1, 2, 3, 4, ...]:  # Add valid shape types
+            if shape_type not in [1, 2, 3, 4]:
                 print(f"Error: Invalid shape type '{shape_type}'. Skipping shape.")
                 continue
 
@@ -83,28 +63,16 @@ def json_to_pptx(json_path, pptx_path):
             if 'text' in shape_data and shape.has_text_frame:
                 shape.text_frame.text = shape_data['text']
 
-    # Save the new PowerPoint presentation
     presentation.save(pptx_path)
 
-
-
 def main():
-    currentdir = os.getcwd() 
-    pptx_file_path = f"{currentdir}/template.pptx"
-    pptx_output_test = f"{currentdir}/output.json"
-    pptx_output = f"{currentdir}/output.pptx"
-    presentation = Presentation(pptx_file_path)
-    first_slide = presentation.slides[0]
-    
+    current_dir = os.getcwd()
+    template_pptx_path = os.path.join(current_dir, "template.pptx")
+    xml_output_path = os.path.join(current_dir, "output.xml")
+    converted_pptx_path = os.path.join(current_dir, "converted.pptx")
 
-    slide_text = extract_text_from_slide(first_slide)
-    print("Text from the first slide:")
-    print(slide_text)
-
-    pptx_to_json(pptx_file_path, pptx_output_test)
-    json_to_pptx(pptx_output_test, pptx_output)
-    print("Done!")
-
+    pptx_to_xml(template_pptx_path, xml_output_path)
+    xml_to_pptx(xml_output_path, converted_pptx_path)
 
 if __name__ == "__main__":
     main()
