588a2521ac8074dbb88d8447798932bbb0b113a2
Alexander peczon <97857268+BakedSoups@users.noreply.github.com>
2023-12-28 13:22:53 -0800

    working title and qr code
    working title and qr code functionality added (spacing is wrong)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pptx import Presentation
-import os 
+from pptx.util import Inches, Pt
+import os
 
 def extract_text_from_slide(slide):
     text = ""
@@ -11,44 +12,65 @@ def extract_text_from_slide(slide):
 def find_class_name(slide):
     for i in range(0, len(slide.shapes)):
         if hasattr(slide.shapes[i], "text"):
-            print(slide.shapes[i].text)
             if slide.shapes[i].text == "[CLASS + NUMBER]":
-                return i +1 
+                return i + 1 
+    return None
+
+def change_class_name(slide, class_name):
+    for i in range(0, len(slide.shapes)):
+        if hasattr(slide.shapes[i], "text"):
+            if slide.shapes[i].text == "[CLASS + NUMBER]":
+                slide.shapes[i].text = class_name
+                return True
     return False
 
-def change_class_name(slide, classPos,  new_text): 
-    if presentation.slide[0].shapes[classPos].text != "":
-        presentation.slide[0].shapes[classPos].text = new_text
-       
-def copy_slides(source_presentation, destination_presentation):
-    for slide in source_presentation.slides:
-        destination_presentation.slides.add_slide(slide)
-
+def clone_slide(prs, slide):
+    new_slide = prs.slides.add_slide(slide.slide_layout)
+    for shape in slide.shapes:
+        if shape.has_text_frame:
+            if shape.text_frame.text:
+                new_shape = new_slide.shapes.add_textbox(
+                    left=shape.left,
+                    top=shape.top,
+                    width=shape.width,
+                    height=shape.height
+                )
+                new_shape.text_frame.text = shape.text_frame.text
+                for paragraph in shape.text_frame.paragraphs:
+                    new_paragraph = new_shape.text_frame.add_paragraph()
+                    new_paragraph.text = paragraph.text
+                    new_paragraph.font.bold = paragraph.font.bold
+                    new_paragraph.font.italic = paragraph.font.italic
+                    new_paragraph.font.size = Pt(paragraph.font.size.pt) if paragraph.font.size else Pt(18)
+                    new_paragraph.font.color.rgb = paragraph.font.color.rgb if paragraph.font.color else (0, 0, 0)  # Default to black color if None
+        elif shape.has_image_frame:
+            image_path = shape.image.filename
+            new_shape = new_slide.shapes.add_picture(
+                image_path,
+                left=shape.left,
+                top=shape.top,
+                width=shape.width,
+                height=shape.height
+            )
+    return new_slide
 
 def main():
     current_dir = os.getcwd()
+
     template_file_path = f"{current_dir}/template.pptx"
     template_presentation = Presentation(template_file_path)
-    output_presentation = Presentation()
-    copy_slides(template_presentation, output_presentation)
-    first_slide = output_presentation.slides[0]
-    slide_text = extract_text_from_slide(first_slide)
+
+    template_first_slide = template_presentation.slides[0]
+    template_slide_text = extract_text_from_slide(template_first_slide)
+
+    change_class_name(template_first_slide, "CS 101")
+
+    new_presentation = Presentation()
+
+    # Clone the modified slide to the new presentation
+    clone_slide(new_presentation, template_first_slide)
+
+    new_presentation.save(f"{current_dir}/new_presentation.pptx") 
     
-    print("Text from the first slide:")
-    print(slide_text)
-
-    # Find the position of the class name shape
-    class_pos = find_class_name(first_slide)
-
-    if not class_pos:
-        raise Exception("Class name not found")
-    else:
-        # Change the class name on the first slide
-        change_class_name(first_slide, class_pos, "CS 101")
-
-    # Save the modified presentation
-    output_file_path = f"{current_dir}/output.pptx"
-    output_presentation.save(output_file_path)
-
 if __name__ == "__main__":
     main()
